1fedcc042dd26029c76399770c1b2beb4a4e3856
Sammy Ha <sammy.ha@kingfisher.com>
2018-09-26 19:58:40 +0100

    114 cases for ingredients

--- src/components/BurgerIngredient/BurgerIngredient.js
+++ src/components/BurgerIngredient/BurgerIngredient.js
@@ -17,6 +17,16 @@ const burgerIngredient = (props) => {
                 </div>
             );
             break;
+        case('meat'): 
+            ingredient = <div className={classes.Meat}></div>;
+        case('cheese'): 
+            ingredient = <div className={classes.Cheese}></div>;
+        case('salad'): 
+            ingredient = <div className={classes.Salad}></div>;
+        case('bacon'): 
+            ingredient = <div className={classes.Bacon}></div>;
+        default:
+            ingredient = null;
     }
 }
 
