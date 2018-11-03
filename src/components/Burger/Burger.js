ecd1542234f60662a107d689f86c81211a2ad53c
sammy.ha <sammy.ha@kingfisher.com>
2018-11-03 15:24:26 +0000

    120: outputting ing dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import classes from './Burger.css';
-import BurgerIngredient from '../BurgerIngredient/BurgerIngredient';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformIng = Object.keys(props.ingredients)
+        .map(igKey => {
+            console.log('igkey', igKey)
+            console.log('Object.keys(props.ingredients)', Object.keys(props.ingredients))
+            console.log('props.ingredients[igKey]', props.ingredients[igKey])
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+                console.log('igKey + i', igKey + i)
+                console.log('i', i)
+                // console.log('key', key)
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformIng}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
