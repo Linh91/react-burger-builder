f713914ebaa4893946e4bffb90b2439f6907b0cf
sammy.ha <sammy.ha@kingfisher.com>
2018-11-05 20:05:05 +0000

    129: showing & hiding modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,12 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
+  }
+
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
   }
 
   updatePurchaseState (ingredients) {
@@ -79,7 +84,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -88,7 +93,8 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
-          purchasable={this.state.purchasable}/>
+          purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}/>
       </Aux>
     )
   }
