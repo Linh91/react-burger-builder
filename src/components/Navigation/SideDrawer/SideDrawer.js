f1514db36481d98bb95f3daeb403e1997dba3fa7
sammy.ha <sammy.ha@kingfisher.com>
2018-11-08 17:28:42 +0000

    140: side drawer opens and closes

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,26 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationsItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationsItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationsItems />
-      </nav>
-    </div>
+    </Aux>
   )
 }
 
